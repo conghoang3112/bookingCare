058322f1a5c7740e21008d1cb41232886322e8af
Cong Hoang <conghoang3112xxx@gmail.com>
2024-02-22 11:00:59 +0700

    write and read file image with sequelize and mySql BLOB

--- src/services/userService.js
+++ src/services/userService.js
@@ -118,7 +118,8 @@ let createNewUser = (data) => {
                     phoneNumber: data.phoneNumber,
                     gender: data.gender,
                     roleId: data.roleId,
-                    positionId: data.positionId
+                    positionId: data.positionId,
+                    image: data.avatar
                 })
                 resolve({
                     errCode: 0,
@@ -176,6 +177,10 @@ let editUserData = (data) => {
                 user.positionId = data.positionId;
                 user.gender = data.gender;
                 user.phoneNumber = data.phoneNumber;
+                if (data.image) {
+                    user.image = data.avatar;
+                }
+                user.image = data.avatar;
 
                 await user.save();
                 resolve({
